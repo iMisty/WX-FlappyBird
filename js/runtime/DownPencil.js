0009ebfac181a6e496c8cfb93396b5de6988e79b
Miya <qianwumeng_2cyndi@qq.com>
2018-11-18 15:26:06 +0800

    Update DownPencil.js

--- js/runtime/DownPencil.js
+++ js/runtime/DownPencil.js
@@ -1,3 +1,15 @@
-export class DownPencil {
+import {Pencil} from "./Pencil.js";
+import {Sprite} from "../base/Sprite.js";
 
+export class DownPencil extends Pencil {
+    constructor(top) {
+        const image = Sprite.getImage('PencilDown');
+        super(image, top);
+    }
+
+    draw() {
+        let gap = window.innerHeight / 5;
+        this.y = this.top + gap;
+        super.draw();
+    }
 }
