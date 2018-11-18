139d63585739b46b7b094fafe8f2946cf4221501
Miya <qianwumeng_2cyndi@qq.com>
2018-11-18 15:25:58 +0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,15 +1,36 @@
 // 初始化
 import {ResourceLoader} from "./js/base/ResourcesLoader.js";
+import {BackGround} from "./js/runtime/BackGround.js";
+import {Land} from "./js/runtime/Land.js";
+import {DataStore} from "./js/base/DataStore.js";
+import {Director} from "./js/Director.js";
 
 export class Main {
     constructor() {
         this.canvas =document.getElementById('canvas');
         this.ctx = this.canvas.getContext('2d');
+        this.dataStore = DataStore.getInstance();
+        this.director = Director.getInstance();
         const loader = ResourceLoader.create();
         loader.onLoaded(map => this.onResourceFirstLoaded(map));
     }
 
     onResourceFirstLoaded(map) {
+        this.dataStore.ctx = this.ctx;
+        this.dataStore.res = map;
+        this.init();
+    }
 
+    init() {
+        // 重置游戏
+        this.director.isGameOver = false;
+
+        this.dataStore
+            .put('pencils', [])
+            .put('background',BackGround)
+            .put('land',Land);
+
+        this.director.createPencil();
+        Director.run();
     }
 }
