dadcc03f76151ea7ed75e685332018f4317e99ed
Miya <qianwumeng_2cyndi@qq.com>
2018-11-18 15:25:56 +0800

    Update DataStore.js

--- js/base/DataStore.js
+++ js/base/DataStore.js
@@ -1,4 +1,33 @@
 // 变量缓存器
 export class DataStore{
 
+    static getInstance() {
+        if(!DataStore.instance) {
+            DataStore.instance = new DataStore();
+        }
+        return DataStore.instance;
+    }
+
+    constructor() {
+        this.map = new Map();
+    }
+
+    put(key, value) {
+        if(typeof value === 'function') {
+            value = new value();
+        }
+        this.map.set(key, value);
+        return this;
+    }
+
+    get(key) {
+        return this.map.get(key);
+    }
+
+    destory() {
+        for (let value of this.map.values()) {
+            value = null;
+        }
+    }
+
 }
