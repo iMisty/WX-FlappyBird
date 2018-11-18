838afa48312f68bf0ea81a5b3a55ce2925a071db
Miya <qianwumeng_2cyndi@qq.com>
2018-11-18 15:26:08 +0800

    Update BackGround.js

--- js/runtime/BackGround.js
+++ js/runtime/BackGround.js
@@ -1,3 +1,12 @@
-export class BackGround {
+import {Sprite} from "../base/Sprite.js";
 
+export class BackGround extends Sprite {
+    constructor() {
+        const image = Sprite.getImage('background');
+        super(image,
+            0,0,
+            image.width,image.height,
+            0,0,
+            window.innerWidth,window.innerHeight);
+    }
 }
