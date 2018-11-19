e251739326ff61c17f9aa8a33c9da0048b4ead55
Miya <qianwumeng_2cyndi@qq.com>
2018-11-19 17:13:08 +0800

    Update BackGround.js

--- js/runtime/BackGround.js
+++ js/runtime/BackGround.js
@@ -1,12 +1,13 @@
 import {Sprite} from "../base/Sprite.js";
+import {DataStore} from "../base/DataStore.js";
 
 export class BackGround extends Sprite {
     constructor() {
         const image = Sprite.getImage('background');
         super(image,
-            0,0,
-            image.width,image.height,
-            0,0,
-            window.innerWidth,window.innerHeight);
+            0, 0,
+            image.width, image.height,
+            0, 0,
+            DataStore.getInstance().canvas.width, DataStore.getInstance().canvas.height);
     }
 }
