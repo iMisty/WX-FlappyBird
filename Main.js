b1d34436df95a0501f29fcb245f405d925459d7e
Miya <qianwumeng_2cyndi@qq.com>
2018-11-19 17:12:57 +0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,14 +1,18 @@
-// 初始化
-import {ResourceLoader} from "./js/base/ResourcesLoader.js";
+import {ResourceLoader} from "./js/base/ResourceLoader.js";
 import {BackGround} from "./js/runtime/BackGround.js";
-import {Land} from "./js/runtime/Land.js";
 import {DataStore} from "./js/base/DataStore.js";
 import {Director} from "./js/Director.js";
+import {Land} from "./js/runtime/Land.js";
 import {Birds} from "./js/player/Birds.js";
+import {StartButton} from "./js/player/StartButton.js";
+import {Score} from "./js/player/Score.js";
+import {ApiExamples} from "./js/ApiExamples.js";
 
 export class Main {
+
     constructor() {
-        this.canvas =document.getElementById('canvas');
+
+        this.canvas = wx.createCanvas();
         this.ctx = this.canvas.getContext('2d');
         this.dataStore = DataStore.getInstance();
         this.director = Director.getInstance();
@@ -16,36 +20,45 @@ export class Main {
         loader.onLoaded(map => this.onResourceFirstLoaded(map));
     }
 
+    createBackgroundMusic() {
+        const bgm = wx.createInnerAudioContext();
+        bgm.autoplay = true;
+        bgm.loop = true;
+        bgm.src = 'audios/bgm.mp3';
+    }
+
     onResourceFirstLoaded(map) {
+        this.dataStore.canvas = this.canvas;
         this.dataStore.ctx = this.ctx;
         this.dataStore.res = map;
+        this.createBackgroundMusic();
+        const examples = new ApiExamples();
+
         this.init();
     }
 
     init() {
-        // 重置游戏
-        this.director.isGameOver = false;
 
+        this.director.isGameOver = false;
         this.dataStore
             .put('pencils', [])
-            .put('background',BackGround)
-            .put('land',Land)
-            .put('birds',Birds);
+            .put('background', BackGround)
+            .put('land', Land)
+            .put('birds', Birds)
+            .put('score', Score)
+            .put('startButton', StartButton);
         this.registerEvent();
         this.director.createPencil();
-        Director.run();
+        this.director.run();
     }
 
     registerEvent() {
-        this.canvas.addEventListener('touchstart', e => {
-            // 屏蔽原生事件冒泡
-            e.preventDefault();
-            if(this.director.isGameOver){
+        wx.onTouchStart(() => {
+            if (this.director.isGameOver) {
                 this.init();
             } else {
                 this.director.birdsEvent();
             }
         });
     }
-
 }
