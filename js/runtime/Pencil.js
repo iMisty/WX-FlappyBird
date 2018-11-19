f16df285912b3818b4238fd3e223a42168ce6fd0
Miya <qianwumeng_2cyndi@qq.com>
2018-11-19 17:13:01 +0800

    Update Pencil.js

--- js/runtime/Pencil.js
+++ js/runtime/Pencil.js
@@ -1,22 +1,25 @@
-import {Sprite} from "../base/Sprite.js";
-import {Director} from "../Director.js";
+import { Sprite } from "../base/Sprite.js";
+import {DataStore} from "../base/DataStore.js";
 
-export class Pencil extends Sprite{
-    constructor(image, top) {
-        super(image,0,0,
-            image.width,image.height,
-            window.innerWidth,0,
-            image.width,image.height);
-        this.top = top;
-    }
+export class Pencil extends Sprite {
 
-    draw() {
-        this.x = this.x - Director.getInstance().landSpeed;
-        super.draw(this.img,
-            0,0,
-            this.width,this.height,
-            this.x,this.y,
-            this.width,this.height);
-    }
+  constructor(image, top) {
+    super(image,
+      0, 0,
+      image.width, image.height,
 
+      DataStore.getInstance().canvas.width, 0,
+      image.width, image.height);
+    this.top = top;
+    this.moveSpeed = 2;
+  }
+
+  draw() {
+    this.x = this.x - this.moveSpeed;
+    super.draw(this.img,
+      0, 0,
+      this.width, this.height,
+      this.x, this.y,
+      this.width, this.height)
+  }
 }
