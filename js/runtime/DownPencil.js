128fa40c677f965975bedca506f9e523495a0902
Miya <qianwumeng_2cyndi@qq.com>
2018-11-19 17:13:06 +0800

    Update DownPencil.js

--- js/runtime/DownPencil.js
+++ js/runtime/DownPencil.js
@@ -1,15 +1,18 @@
+//下半部分铅笔
 import {Pencil} from "./Pencil.js";
 import {Sprite} from "../base/Sprite.js";
+import {DataStore} from "../base/DataStore.js";
 
 export class DownPencil extends Pencil {
     constructor(top) {
-        const image = Sprite.getImage('PencilDown');
+        const image = Sprite.getImage('pencilDown');
         super(image, top);
     }
 
     draw() {
-        let gap = window.innerHeight / 5;
+        let gap = DataStore.getInstance().canvas.height / 5;
         this.y = this.top + gap;
         super.draw();
     }
+
 }
