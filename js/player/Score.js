1ddee9fabfa892657e6565c5e7025058fe65c253
Miya <qianwumeng_2cyndi@qq.com>
2018-11-19 17:13:13 +0800

    Update Score.js

--- js/player/Score.js
+++ js/player/Score.js
@@ -1,3 +1,22 @@
+import {DataStore} from "../base/DataStore.js";
+
 export class Score {
 
+    constructor() {
+        this.ctx = DataStore.getInstance().ctx;
+        this.scoreNumber = 0;
+
+        this.isScore = true;
+    }
+
+    draw() {
+        this.ctx.font = '24px Arial';
+        this.ctx.fillStyle = '#557eff';
+        this.ctx.fillText(
+            this.scoreNumber,
+            DataStore.getInstance().canvas.width / 2,
+            DataStore.getInstance().canvas.height / 18,
+            888
+        );
+    }
 }
